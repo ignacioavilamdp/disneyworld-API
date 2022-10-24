ef6dfa528a8cf5d830943dc8b594d962686f7ba0
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-10-24 11:03:24 -0300

    dev : Exceptions added

---
+++ src/main/java/com/challenge/disneyworld/exceptions/InvalidIdException.java
@@ -0,0 +1,9 @@
+package com.challenge.disneyworld.exceptions;
+
+public class InvalidIdException extends RuntimeException{
+
+    public InvalidIdException(String message){
+        super(message);
+    }
+
+}
