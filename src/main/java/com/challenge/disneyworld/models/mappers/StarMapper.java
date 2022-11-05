8a12dcf39f83b4ee8496960d029cb38c1033cf47
Ignacio Avila <ignacioavilamdp@gmail.com>
2022-11-05 14:04:17 -0300

    dev : modified the way the Star and Content mapper work.

--- src/main/java/com/challenge/disneyworld/models/mappers/StarMapper.java
+++ src/main/java/com/challenge/disneyworld/models/mappers/StarMapper.java
@@ -20,15 +20,6 @@ import java.util.List;
 @Component
 public class StarMapper {
 
-    private final StarRepository starRepository;
-    private final ContentRepository contentRepository;
-
-    @Autowired
-    public StarMapper(StarRepository starRepository, ContentRepository contentRepository) {
-        this.starRepository = starRepository;
-        this.contentRepository = contentRepository;
-    }
-
     /**
      * Returns a new {@link StarBaseDTO} instance using the data contained
      * in the {@link Star} given.
@@ -75,72 +66,4 @@ public class StarMapper {
         return dto;
     }
 
-    /**
-     * Returns a new {@link Star} instance using the data contained
-     * in the {@link StarDetailDTO} given.
-     *
-     * <p>Precondition: The dto must not be null.
-     *
-     * @param dto the dto containing the data to populate a new star instance
-     * from.
-     * @return a new star instance populated with values extracted from the dto
-     * instance given.
-     */
-    public Star detailDTOToEntity(StarDetailDTO dto){
-        if (dto.getName() == null)
-            throw new InvalidDTOException("No name passed. Name is mandatory.");
-
-        if (starRepository.existsByName(dto.getName()))
-            throw new InvalidDTOException("There is already a character with the same name (" + dto.getName() + "). No duplicates allowed");
-
-        return copyDataFromDTO(new Star(), dto);
-    }
-
-    /**
-     * Returns the same {@link Star} instance given, with its data values
-     * updated using the {@link StarDetailDTO} given.
-     *
-     * <p>Precondition: The star and the dto must not be null.
-     *
-     * @param star the star instance which data wants to be updated.
-     * @param dto the dto instance containing the data to update the star
-     * instance from.
-     * @return the given star instance populated with values extracted from the
-     * dto instance given.
-     */
-    public Star updateEntityFromDTO(Star star, StarDetailDTO dto){
-        if (star.getId() != dto.getId())
-            throw new InvalidDTOException("The id in the payload does not match the id in the URI. " +
-                    "Are you trying to modify the id? This is not allowed.");
-
-        if (dto.getName() == null)
-            throw new InvalidDTOException("No name passed. Name is mandatory.");
-
-        Star starByDTOName = starRepository.getByName(dto.getName());
-        if (starByDTOName != null && starByDTOName.getId() != star.getId())
-            throw new InvalidDTOException("There is already a character with the same name (" + dto.getName() + "). No duplicates allowed.");
-
-        return copyDataFromDTO(star, dto);
-    }
-
-    private Star copyDataFromDTO(Star star, StarDetailDTO dto){
-        star.setName(dto.getName());
-        star.setImage(dto.getImage());
-        star.setAge(dto.getAge());
-        star.setHistory(dto.getHistory());
-        star.setWeight(dto.getWeight());
-
-        /*
-         * We need to remove all the relations between the content and its stars.
-         * For each name in the dto we need to check if the content entity exists.
-         */
-        star.getContents().clear();
-        for (String contentTitle : dto.getContents()){
-            Content content = contentRepository.getByTitle(contentTitle);
-            if (content == null)
-                throw new InvalidDTOException("There is no movie with title: " + contentTitle + ". Please, add the movie first.");
-            star.getContents().add(content);
-        }
-        return star;
-    }
 }
